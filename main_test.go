00974c80804eee2f1b5551ceed041d88a7f2edbc
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-04-07 00:56:36 +0900

    added test code

--- main_test.go
+++ main_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"testing"
+	"os"
+)
+
+func TestMain(m *testing.M){
+	println("Before all...")
+
+	code := m.Run()
+
+	println("After all...")
+
+	os.Exit(code)
+}
+
+func TestChHomeDirSuccess(t *testing.T){
+}
+
+func TestChHomeDirFailed(t *testing.T){
+
+}
+
+func TestHandleCtrlCSuccess(t *testing.T){
+
+}
+
+func TestHandleCtrlCFailed(t *testing.T){
+
+}
+
+func TestEncodingJsonSuccess(t *testing.T){
+
+}
+
+func TestEncodingJsonFailed(t *testing.T){
+
+}
+
+func TestGetServicesInfoSuccess(t *testing.T){
+
+}
+
+func TestGetServicesInfoFailed(t *testing.T){
+
+}
